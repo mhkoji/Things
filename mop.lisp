b2ca34b65919a1ae515802e3da4219e1b5eb8ee2
mhkoji <mhkoji2+git@gmail.com>
2019-11-17 12:05:03 +0900

    Update mop

--- mop.lisp
+++ mop.lisp
@@ -5,17 +5,29 @@
 (defgeneric show (x))
 
 #+sbcl
-(let ((method
-       (multiple-value-bind (meth args)
-           (sb-mop:make-method-lambda
-            (sb-mop:class-prototype (find-class 'standard-generic-function))
-            (sb-mop:class-prototype (find-class 'standard-method))
-            '(lambda (x) (format t "Show string of ~A~%" x))
-            nil)
-         (apply #'make-instance 'standard-method
-                :function (compile nil meth)
-                :lambda-list (list 'x)
-                :specializers (list (find-class 'string))
-                args))))
-    (add-method (sb-mop:ensure-generic-function 'show) method)
-    (show "Hello!"))
+(progn
+  (defun call-with-context (callback)
+    (let ((method
+           (multiple-value-bind (meth args)
+               (sb-mop:make-method-lambda
+                (sb-mop:class-prototype (find-class
+                                         'standard-generic-function))
+                (sb-mop:class-prototype (find-class
+                                         'standard-method))
+                '(lambda (x)
+                  (format t "Show string of ~A~%" x))
+                nil)
+             (apply #'make-instance 'standard-method
+                    :function (compile nil meth)
+                    :lambda-list (list 'x)
+                    :specializers (list (find-class 'string))
+                    args))))
+      (let ((gf-show (sb-mop:ensure-generic-function 'show)))
+        ;; TODO: need multi-thread support to protect the gf-show object?
+        (add-method gf-show method)
+        (unwind-protect (funcall callback)
+          (remove-method gf-show method)))))
+
+  (call-with-context
+   (lambda ()
+     (show "Hello!"))))
