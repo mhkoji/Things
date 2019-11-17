011c8c182937e2ac2bff7c678dabcb0dc4f7d266
mhkoji <mhkoji2+git@gmail.com>
2019-11-17 12:26:09 +0900

    Add DCI-like code

--- mop.lisp
+++ mop.lisp
@@ -31,3 +31,45 @@
   (call-with-context
    (lambda ()
      (show "Hello!"))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; The value printer role
+(defgeneric get-value (value-printer))
+
+(defun print-value (value-printer)
+  (let ((v (get-value value-printer)))
+    (format t "Value printer prints: ~A~%" v)))
+
+
+;;; The value of a number object
+(defun get-number-value (x)
+  (format nil "The value of number [~A]" x))
+
+
+#+sbcl
+(progn
+  (defmacro with-print-value-context ((class &key get-value) &rest body)
+    `(let ((method
+            (multiple-value-bind (meth args)
+                (sb-mop:make-method-lambda
+                 (sb-mop:class-prototype (find-class
+                                          'standard-generic-function))
+                 (sb-mop:class-prototype (find-class
+                                          'standard-method))
+                 '(lambda (x)
+                   (funcall ,get-value x))
+                 nil)
+              (apply #'make-instance 'standard-method
+                     :function (compile nil meth)
+                     :lambda-list (list 'x)
+                     :specializers (list (find-class ',class))
+                     args))))
+       (let ((gf (sb-mop:ensure-generic-function 'get-value)))
+         (add-method gf method)
+         (unwind-protect (progn ,@body)
+           (remove-method gf method)))))
+
+  ;;; Make a number object play the value printer role
+  (with-print-value-context (number :get-value #'get-number-value)
+    (print-value 10)))
